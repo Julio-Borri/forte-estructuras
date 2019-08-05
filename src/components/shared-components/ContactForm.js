b2ce459a7eb729a77bac65d88ad2275398a0daba
Julio-Borri <juliodborri@gmail.com>
2019-08-05 14:51:35 -0300

    initial commit

--- src/components/shared-components/ContactForm.js
+++ src/components/shared-components/ContactForm.js
@@ -1,24 +1,94 @@
 import React from 'react';
 import { Form, Button, } from 'react-bootstrap';
 import data from '../../data';
+import PropTypes from 'prop-types';
 
-export default function ContactForm() {
-  return (
-    <Form style={{ maxWidth: "700px", marginBottom: "1110px", }} className="m-auto border p-4">
-      <Form.Group>
-        <Form.Label>{data.formulario.form1.label}</Form.Label>
-        <Form.Control type="text" placeholder={data.formulario.form1.placeHolder} />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>{data.formulario.form2.label}</Form.Label>
-        <Form.Control type="email" placeholder={data.formulario.form2.placeHolder} />
-        <Form.Text className="text-muted">{data.formulario.form2.helpText}</Form.Text>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>{data.formulario.form3.label}</Form.Label>
-        <Form.Control as="textarea" rows="5" placeholder={data.formulario.form3.placeHolder} />
-      </Form.Group>
-      <Button variant="outline-dark" type="submit" block >Enviar</Button>
-    </Form>
-  )
+export default class ContactForm extends React.Component {
+  state = {
+    feedback: '',
+    formSubmitted: false,
+  };
+
+  handleCancel = this.handleCancel.bind(this);
+  handleChange = this.handleChange.bind(this);
+  handleSubmit = this.handleSubmit.bind(this);
+
+  static sender = 'juliodborri@gmail.com';
+
+  handleCancel() {
+    this.setState({
+      feedback: ''
+    });
+  }
+
+  handleChange(event) {
+    this.setState({
+      feedback: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const {
+      REACT_APP_EMAILJS_TEMPLATEID: template,
+      REACT_APP_EMAILJS_RECEIVER: receiverEmail,
+    } = this.props.env;
+
+    this.sendFeedback(
+      template,
+      this.sender,
+      receiverEmail,
+      this.state.feedback
+    );
+
+    this.setState({
+      formSubmitted: true
+    });
+  }
+
+  sendFeedback(templateId, senderEmail, receiverEmail, feedback) {
+    window.emailjs
+    //EmailJS's send() function, 
+      .send('mailgun', templateId, {
+        senderEmail,
+        receiverEmail,
+        feedback
+      })
+      .then(res => {
+        this.setState({
+          formEmailSent: true
+        });
+      })
+      // Handle errors here however you like
+      .catch(err => console.error('Failed to send feedback. Error: ', err));
+  }
+
+
+  render() {
+    return (
+      <Form style={{ maxWidth: "700px", marginBottom: "1110px", }} className="m-auto border p-4" onSubmit={this.handleSubmit}>
+        <Form.Group>
+          <Form.Label>{data.formulario.form1.label}</Form.Label>
+          <Form.Control type="text" placeholder={data.formulario.form1.placeHolder} id="name-entry" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>{data.formulario.form2.label}</Form.Label>
+          <Form.Control type="email" placeholder={data.formulario.form2.placeHolder} id="email-entry" />
+          <Form.Text className="text-muted">{data.formulario.form2.helpText}</Form.Text>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>{data.formulario.form3.label}</Form.Label>
+          <Form.Control as="textarea" rows="5" placeholder={data.formulario.form3.placeHolder} id="feedback-entry" />
+        </Form.Group>
+        <Button variant="outline-dark" type="submit" value="Submit" block >Enviar</Button>
+      </Form>
+    )
+  }
+
+
 }
+
+ContactForm.propTypes = {
+  env: PropTypes.object.isRequired
+};
